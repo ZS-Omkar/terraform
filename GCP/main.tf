6315decd3b8f2d731f42c41aa389fc895028b040
ZS-Omkar <omkar@zelarsoft.com>
2021-10-08 16:46:54 +0530

    ss

--- GCP/main.tf
+++ GCP/main.tf
@@ -15,7 +15,18 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform"
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
   machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
 }
