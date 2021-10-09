d84378ac622d33f3946f72b240e7b00fc04eddde
ZS-Omkar <omkar@zelarsoft.com>
2021-10-09 23:44:09 +0530

    ss

---
+++ maps/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0eb5f3f64b10d3e0e"
+  instance_type = "t2.micro"
+  count = var.instance_count
+  tags = {
+    Name = "terra-${count.index + 1}"
+  }
+}
+variable "instance_count" {
+  default = "2"
+}
+
